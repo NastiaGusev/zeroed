f852df7ffd32451a36045281702871423acbe53b
NastiaGusev <gusev.nastia@gmail.com>
2026-05-03 11:38:01 +0300

    feat: initial NestJS + Prisma setup with Neon DB

--- backend/dist/src/generated/prisma/models.d.ts
+++
@@ -1,7 +0,0 @@
-export type * from './models/User.js';
-export type * from './models/Group.js';
-export type * from './models/GroupMember.js';
-export type * from './models/Expense.js';
-export type * from './models/ExpenseSplit.js';
-export type * from './models/Settlement.js';
-export type * from './commonInputTypes.js';
